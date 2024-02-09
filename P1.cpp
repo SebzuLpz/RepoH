3c01040f59721df1d92b006f2ac2f93b97b86b41
Juan Sebastián López Parra <147025620+SebzuLpz@users.noreply.github.com>
2024-02-08 22:41:55 -0500

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
 using namespace std;
-int main (){
+int main() {
     int cantidad;
-    cout<<"Ingrese cantidad de numeros: "<<endl;
-    cin>>cantidad;
-    if (cantidad<=0){
-        cout<<"La cantidad de numeros debe ser mayor a cero."<<endl;
-        cout<<"Ingrese cantidad de numeros: "<<endl;
-    cin>>cantidad;
+    cout << "Ingrese cantidad de numeros: " << endl;
+    cin >> cantidad;
+    while (cantidad <= 0) {
+        cout << "La cantidad de numeros debe ser mayor a cero." << endl;
+        cout << "Ingrese cantidad de numeros: " << endl;
+        cin >> cantidad;
     }
-    double*numeros=new double[cantidad];
-    cout<<"Ingrese los numeros uno por uno: "<<endl;
-    for (int i=0; i<cantidad;++i){
-        cout<<"Numero "<<i+1<<" : "<<endl;
-        cin>>numeros[i];
-    }
-    double suma=0;
+    double suma = 0;
     double prom;
-    int maximo=numeros[0];
-    int minimo=numeros[0];
-    for (int i = 0; i < cantidad; ++i) {
-        suma += numeros[i];
-        if (numeros[i] > maximo) {
-            maximo = numeros[i];
+    double maximo, minimo;
+    cout << "Ingrese el numero 1: " << endl;
+    cin >> maximo;
+    suma += maximo;
+    minimo = maximo;
+    for (int i = 1; i < cantidad; ++i) {
+        double numero;
+        cout << "Ingrese el numero " << i + 1 << ": " << endl;
+        cin >> numero;
+        suma += numero;
+        if (numero > maximo) {
+            maximo = numero;
         }
-        if (numeros[i] < minimo) {
-            minimo = numeros[i];
+        if (numero < minimo) {
+            minimo = numero;
         }
     }
     prom = suma / cantidad;
